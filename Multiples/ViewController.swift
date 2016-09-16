507f142386d65e663047c26cc53a9efe483d0ee7
Robert <RobLowe1992@yahoo.com>
2016-09-16 16:39:36 -0400

    App is functioning properly

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -9,17 +9,86 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    //Properties
+    var finalMUltiple = 0
+    var currentMultiple = 0
+    var initialMultiple = 0
+    
+    
+    //Outlets
+    @IBOutlet weak var mLogo: UIImageView!
+    @IBOutlet weak var txtField: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var pencilBtn: UIButton!
+    @IBOutlet weak var multLbl: UILabel!
+    
+    //Actions
+    
+    //Hides 3 outlets and reveals 2
+    @IBAction func onPlayBtn(sender: UIButton!) {
+        if txtField.text != nil && txtField.text != "" {
+            
+            mLogo.hidden = true
+            txtField.hidden = true
+            playBtn.hidden = true
+            
+            pencilBtn.hidden = false
+            multLbl.hidden = false
+            
+            initialMultiple = Int(txtField.text!)!
+            currentMultiple == initialMultiple
+            
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Computes the equation
+    @IBAction func onPencilClick(sender: UIButton!) {
+        updateLbl()
+        currentMultiple += initialMultiple
+   
+        addMultiples()
+        
+        if ifGameOver() {
+            restartGame()}
+        
     }
-
-
+    
+    
+    //Updates the text on the label
+    func updateLbl() {
+        multLbl.text = ("\(initialMultiple) + \(currentMultiple) = \(finalMUltiple)")
+        
+    }
+    //Checks to see if the game is over
+    func ifGameOver() -> Bool {
+        if currentMultiple >= initialMultiple * 10 {
+            return true
+        }
+            return false
+    }
+    //Restarts the game
+    func restartGame() {
+        
+        txtField.text = ""
+        initialMultiple = 0
+        currentMultiple = 0
+        finalMUltiple = 0
+        
+        mLogo.hidden = false
+        txtField.hidden = false
+        playBtn.hidden = false
+        
+        pencilBtn.hidden = true
+        multLbl.hidden = true
+        
+        updateLbl()
+    }
+    func addMultiples() {
+         finalMUltiple = currentMultiple + initialMultiple
+    }
 }
+
+
+
 
